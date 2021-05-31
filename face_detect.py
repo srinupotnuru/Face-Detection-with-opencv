9405a4a994dc4c2b95368233f4f793954d1feda9
srinupotnuru <iamsrinu3@gmail.com>
2021-06-01 01:34:25 +0530

    initial commit

---
+++ face_detect.py
@@ -0,0 +1,14 @@
+import cv2
+face_detect=cv2.CascadeClassifier('classifier.xml')
+
+img=cv2.imread('karthik.jpeg')
+
+gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+faces=face_detect.detectMultiScale(gray,1.1,4)
+
+for (x,y,w,h) in faces:
+    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+
+    cv2.imshow('img',img)
+    cv2.waitKey()
